2394ecdb3c6575858b206ed61266a9d92619e86d
Shikha Gupta <shikha@mobrilz.agency>
2024-10-28 16:31:29 +0530

    contract details fixed

--- resources/views/backend/completecontract/show.blade.php
+++ resources/views/backend/completecontract/show.blade.php
@@ -4,216 +4,91 @@
     <div class="card">
         <div class="d-flex justify-content-between">
             <div class="align-self-center">
-                <h4 class="card-title p-1">
-                    Completed Contract Details
-                </h4>
+                <h4 class="card-title p-1">Completed Contract Details</h4>
             </div>
-            <a href="{{ route('completedcontract.index') }}" class="btn btn-warning"><i class="fas fa-reply fa-fw"></i></a>
+            <a href="{{ route('completedcontract.index') }}" class="btn btn-warning">
+                <i class="fas fa-reply fa-fw"></i>
+            </a>
         </div>
     </div>
+     <br>
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-6">
             <div class="card-body">
-                <table class="table table-bordered">
+                <h5>Booking Information</h5>
+                <div>
                     {{-- Checkout Details --}}
                     @if ($booking->checkout)
-                        <tr>
-                            <th>First Name</th>
-                            <td>{{ $booking->checkout->first_name }}</td>
-                        </tr>
-                        <tr>
-                            <th>Last Name</th>
-                            <td>{{ $booking->checkout->last_name }}</td>
-                        </tr>
-                        <tr>
-                            <th>Email</th>
-                            <td>{{ $booking->checkout->email }}</td>
-                        </tr>
-                        <tr>
-                            <th>Address Line 1</th>
-                            <td>{{ $booking->checkout->address_first }}</td>
-                        </tr>
-                        <tr>
-                            <th>Address Line 2</th>
-                            <td>{{ $booking->checkout->address_last }}</td>
-                        </tr>
+                        <p><strong>First Name:</strong> {{ $booking->checkout->first_name }}</p>
+                        <p><strong>Last Name:</strong> {{ $booking->checkout->last_name }}</p>
+                        <p><strong>Email:</strong> {{ $booking->checkout->email }}</p>
+                        <p><strong>Address Line 1:</strong> {{ $booking->checkout->address_first }}</p>
+                        <p><strong>Address Line 2:</strong> {{ $booking->checkout->address_last }}</p>
                     @else
-                        <tr>
-                            <td colspan="2">
-                                <h5>No Checkout Details Available</h5>
-                            </td>
-                        </tr>
+                        <p><strong>No Checkout Details Available</strong></p>
                     @endif
 
-                    {{-- Contract Details --}}
-                    @if ($booking->contract)
-                        <tr>
-                            <th>Contract ID</th>
-                            <td>{{ $booking->contract->id }}</td>
-                        </tr>
-                        <tr>
-                            <th>License Photo</th>
-                            <td><img src="{{ asset('storage/' . $booking->contract->license_photo) }}" alt="License Photo" style="max-width: 100px;"></td>
-                        </tr>
-                        <tr>
-                            <th>Fuel Level</th>
-                            <td>{{ $booking->contract->fuel_level }}</td>
-                        </tr>
-                        <tr>
-                            <th>Vehicle Damage Comments</th>
-                            <td>{{ $booking->contract->vehicle_damage_comments }}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Signature</th>
-                            <td><img src="{{ asset('storage/' . $booking->contract->customer_signature) }}" alt="Customer Signature" style="max-width: 100px;"></td>
-                        </tr>
-                        <tr>
-                            <th>Vehicle Images</th>
-                            <td><img src="{{ asset('storage/' . $booking->contract->vehicle_images) }}" alt="Vehicle Image" style="max-width: 100px;"></td>
-                        </tr>
+                    {{-- Booking Information --}}
+                    <p><strong>Car Name:</strong> {{ $booking->name }}</p>
+                    <p><strong>Total Price:</strong> {{ $booking->total_price }}</p>
+                    <p><strong>Pick Up Location:</strong> {{ $booking->pickUpLocation }}</p>
+                    <p><strong>Drop Off Location:</strong> {{ $booking->dropOffLocation }}</p>
+                    <p><strong>Status:</strong> {{ ucwords($booking->status) }}</p>
+                    <p><strong>Payment Method:</strong>
+                        @if (isset($tran->payment_method))
+                            <span style="background-color: #b1d994; padding: 5px;">{{ ucwords($tran->payment_method) }}</span>
+                        @else
+                            <span style="background-color: #e8857d; padding: 5px;">Unpaid</span>
+                        @endif
+                    </p>
+
+                    {{-- Additional Booking Information --}}
+                    <p><strong>Daily Price:</strong> {{ $booking->Dprice }}</p>
+                    <p><strong>Weekly Price:</strong> {{ $booking->wprice }}</p>
+                    <p><strong>Monthly Price:</strong> {{ $booking->mprice }}</p>
+                    <p><strong>Day Count:</strong> {{ $booking->day_count }}</p>
+                    <p><strong>Week Count:</strong> {{ $booking->week_count }}</p>
+                    <p><strong>Month Count:</strong> {{ $booking->month_count }}</p>
+                    <p><strong>Additional Driver:</strong> {{ $booking->additional_driver }}</p>
+                    <p><strong>Booster Seat:</strong> {{ $booking->booster_seat }}</p>
+                    <p><strong>Child Seat:</strong> {{ $booking->child_seat }}</p>
+                    <p><strong>Exit Permit:</strong> {{ $booking->exit_permit }}</p>
+                    <p><strong>Pick Up Date:</strong> {{ $booking->pickUpDate }}</p>
+                    <p><strong>Pick Up Time:</strong> {{ $booking->pickUpTime }}</p>
+                    <p><strong>Collection Time:</strong> {{ $booking->collectionTime }}</p>
+                    <p><strong>Collection Date:</strong> {{ $booking->collectionDate }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card-body">
+                <h5>Contract Details</h5>
+                <div >
+                    @if ($booking->is_contract == 2 && $booking->ContractIn)
+                        <p><strong>Postal Code:</strong> {{ $booking->ContractIn->postal_code }}</p>
+                        <p><strong>License Photo:</strong> <img src="{{ asset('storage/' . $booking->ContractIn->license_photo) }}" alt="License Photo" style="max-width: 100px;"></p>
+                        <p><strong>Record Kilometers:</strong> {{ $booking->ContractIn->record_kilometers }}</p>
+                        <p><strong>Fuel Level:</strong> {{ $booking->ContractIn->fuel_level }}</p>
+                        <p><strong>Vehicle Damage Comments:</strong> {{ $booking->ContractIn->vehicle_damage_comments }}</p>
+                        <p><strong>Customer Signature:</strong> <img src="{{ asset('storage/' . $booking->ContractIn->customer_signature) }}" alt="Customer Signature" style="max-width: 100px;"></p>
                     @else
-                        <tr>
-                            <td colspan="2">
-                                <h5>No Contract Details Available</h5>
-                            </td>
-                        </tr>
+                        <p><strong>No Contract Details Available</strong></p>
                     @endif
 
                     {{-- ContractOut Details --}}
                     @if ($booking->contractOut)
-                        <tr>
-                            <th>ContractOut ID</th>
-                            <td>{{ $booking->contractOut->id }}</td>
-                        </tr>
-                        <tr>
-                            <th>Email</th>
-                            <td>{{ $booking->contractOut->email }}</td>
-                        </tr>
-                        <tr>
-                            <th>Fuel Level</th>
-                            <td>{{ $booking->contractOut->fuel_level }}</td>
-                        </tr>
-                        <tr>
-                            <th>Kilometers</th>
-                            <td>{{ $booking->contractOut->record_kilometers }}</td>
-                        </tr>
-                        <tr>
-                            <th>Vehicle Damage Comments</th>
-                            <td>{{ $booking->contractOut->vehicle_damage_comments }}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Signature</th>
-                            <td><img src="{{ asset('storage/' . $booking->contractOut->customer_signature) }}" alt="Customer Signature" style="max-width: 100px;"></td>
-                        </tr>
-                        <tr>
-                            <th>Odometer Image</th>
-                            <td><img src="{{ asset('storage/' . $booking->contractOut->odometer_image) }}" alt="Odometer Image" style="max-width: 100px;"></td>
-                        </tr>
+                        <p><strong>ContractOut ID:</strong> {{ $booking->contractOut->id }}</p>
+                        <p><strong>Email:</strong> {{ $booking->contractOut->email }}</p>
+                        <p><strong>Fuel Level:</strong> {{ $booking->contractOut->fuel_level }}</p>
+                        <p><strong>Kilometers:</strong> {{ $booking->contractOut->record_kilometers }}</p>
+                        <p><strong>Vehicle Damage Comments:</strong> {{ $booking->contractOut->vehicle_damage_comments }}</p>
+                        <p><strong>Customer Signature:</strong> <img src="{{ asset('storage/' . $booking->contractOut->customer_signature) }}" alt="Customer Signature" style="max-width: 100px;"></p>
+                        <p><strong>Odometer Image:</strong> <img src="{{ asset('storage/' . $booking->contractOut->odometer_image) }}" alt="Odometer Image" style="max-width: 100px;"></p>
                     @else
-                        <tr>
-                            <td colspan="2">
-                                <h5>No ContractOut Details Available</h5>
-                            </td>
-                        </tr>
+                        <p><strong>No ContractOut Details Available</strong></p>
                     @endif
-                </table>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card-body">
-                <table class="table table-bordered">
-                    {{-- Booking Information --}}
-                    <tr>
-                        <th>Car Name</th>
-                        <td>{{ $booking->name }}</td>
-                    </tr>
-                    <tr>
-                        <th>Total Price</th>
-                        <td>{{ $booking->total_price }}</td>
-                    </tr>
-                    <tr>
-                        <th>Pick Up Location</th>
-                        <td>{{ $booking->pickUpLocation }}</td>
-                    </tr>
-                    <tr>
-                        <th>Drop Off Location</th>
-                        <td>{{ $booking->dropOffLocation }}</td>
-                    </tr>
-                    <tr>
-                        <th>Status</th>
-                        <td>{{ $booking->status }}</td>
-                    </tr>
-                    <tr>
-                        <th>Payment Method</th>
-                        <td>
-                            @if (isset($tran->payment_method))
-                                <span style="background-color: #b1d994;padding: 5px;">
-                                    {{ ucwords($tran->payment_method) }}
-                                </span>
-                            @else
-                                <span style="background-color: #e8857d;padding: 5px;">
-                                    Unpaid
-                                </span>
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Daily Price</th>
-                        <td>{{ $booking->Dprice }}</td>
-                    </tr>
-                    <tr>
-                        <th>Weekly Price</th>
-                        <td>{{ $booking->wprice }}</td>
-                    </tr>
-                    <tr>
-                        <th>Monthly Price</th>
-                        <td>{{ $booking->mprice }}</td>
-                    </tr>
-                    <tr>
-                        <th>Day Count</th>
-                        <td>{{ $booking->day_count }}</td>
-                    </tr>
-                    <tr>
-                        <th>Week Count</th>
-                        <td>{{ $booking->week_count }}</td>
-                    </tr>
-                    <tr>
-                        <th>Month Count</th>
-                        <td>{{ $booking->month_count }}</td>
-                    </tr>
-                    <tr>
-                        <th>Additional Driver</th>
-                        <td>{{ $booking->additional_driver }}</td>
-                    </tr>
-                    <tr>
-                        <th>Booster Seat</th>
-                        <td>{{ $booking->booster_seat }}</td>
-                    </tr>
-                    <tr>
-                        <th>Child Seat</th>
-                        <td>{{ $booking->child_seat }}</td>
-                    </tr>
-                    <tr>
-                        <th>Exit Permit</th>
-                        <td>{{ $booking->exit_permit }}</td>
-                    </tr>
-                    <tr>
-                        <th>Pick Up Date</th>
-                        <td>{{ $booking->pickUpDate }}</td>
-                    </tr>
-                    <tr>
-                        <th>Pick Up Time</th>
-                        <td>{{ $booking->pickUpTime }}</td>
-                    </tr>
-                    <tr>
-                        <th>Collection Time</th>
-                        <td>{{ $booking->collectionTime }}</td>
-                    </tr>
-                    <tr>
-                        <th>Collection Date</th>
-                        <td>{{ $booking->collectionDate }}</td>
-                    </tr>
-                </table>
+                </div>
             </div>
         </div>
     </div>
