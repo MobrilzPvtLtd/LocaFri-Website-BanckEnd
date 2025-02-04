60c579d79fc8b0cc3e269c74fe2e3ef636ecbb15
Shikha Gupta <shikha@mobrilz.agency>
2025-02-04 12:36:43 +0530

    Update footer.blade.php

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -105,33 +105,7 @@
                         </script>
                     </div> --}}
                     <ul class="header-nav">
-                        <li class="nav-item dropdown">
-                            <button class="btn btn-link nav-link d-flex align-items-center px-2 py-2"
-                                data-coreui-toggle="dropdown" type="button" aria-expanded="false">
-                                <svg class="icon icon-lg icon-tabler icons-tabler-outline icon-tabler-language"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M4 5h7" />
-                                    <path d="M9 3v2c0 4.418 -2.239 8 -5 8" />
-                                    <path d="M5 9c0 2.144 2.952 3.908 6.7 4" />
-                                    <path d="M12 20l4 -9l4 9" />
-                                    <path d="M19.1 18h-6.2" />
-                                </svg>
-                                &nbsp; {{ strtoupper(App::getLocale()) }}
-                            </button>
-                            {{-- <ul class="dropdown-menu dropdown-menu-end" style="--cui-dropdown-min-width: 8rem;">
-                                @foreach (config('app.available_locales') as $locale_code => $locale_name)
-                                    <li>
-                                        <a class="dropdown-item d-flex align-items-center"
-                                            href="{{ route('language.switch', $locale_code) }}">
-                                            {{ $locale_name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul> --}}
-                        </li>
+
                         <li>
                             <div class="language-dropdown-menu" id="language-dropdown-menu">
 
